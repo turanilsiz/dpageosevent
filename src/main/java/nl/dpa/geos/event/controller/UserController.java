0698bc8907a0db7ceed99ebc1b84c24f7e317cf9
turan_ilsiz <turanilsiz@hotmail.com>
2020-04-30 23:42:44 +0200

    First commit to new branch

--- src/main/java/nl/dpa/geos/event/controller/UserController.java
+++ src/main/java/nl/dpa/geos/event/controller/UserController.java
@@ -3,6 +3,7 @@ package nl.dpa.geos.event.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,11 @@ public class UserController {
 			return new ResponseEntity<>(new ExceptionRO("", "Deze gebruiker bestaat al.."), HttpStatus.CONFLICT);
 		}
 		return new ResponseEntity<>(new UserRO(userService.createUser(UserUtil.convertEntityToVO(userVO)), null), HttpStatus.OK);		
+	}
+
+	@GetMapping("hello")
+	public String sayHello(){
+		return "Hello world";
 	}
 
 }
